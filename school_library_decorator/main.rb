b843f14a582498a85a88ec518055dd35ec3e8f4c
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-10-20 17:43:16 +0000

    Implement list books and list people methods

--- school_library_decorator/main.rb
+++ school_library_decorator/main.rb
@@ -15,11 +15,15 @@ class App
     end
 
     def list_books
-        
+        @books.each do |book|
+            puts "Title: \"#{book.title}\", Author: #{book.author}"
+        end
     end
 
     def list_people
-
+        people.each do |person|
+            puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
     end
 
     def create_person
@@ -75,11 +79,6 @@ class App
 
     end
 end
-
-
-
-
-
 
 
 
