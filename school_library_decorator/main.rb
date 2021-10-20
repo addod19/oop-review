eb3ca6aab32c5e75704ff022f936e0e21cf7eae5
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-10-20 18:24:41 +0000

    Implement the create rental mwthod

--- school_library_decorator/main.rb
+++ school_library_decorator/main.rb
@@ -75,14 +75,29 @@ class App
     end
 
     def create_rental
+        puts "Select a book from the following list by number"
 
+        @books.each_with_index do |book, index|
+            puts "#{index} Title \"#{book.title}\", Author: #{book.author}"
+        end
+
+        book_index = gets.chomp.to_i
+
+        puts "\nSelect a person from the following list by number (not id)"
+        @people.each_with_index do |person, index|
+            puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
+
+        person_index = gets.chomp.to_i
+
+        puts "\Date: "
+        date = gets.chomp
+
+        @rentals << Rental.new(date, @books[book_index], @people[person_index])
+        puts "Rental created successfully"
     end
 
     def list_all_rentals_for_person_id
-
-    end
-
-    def list_all_books
 
     end
 end
